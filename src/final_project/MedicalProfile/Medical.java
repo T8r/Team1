e8c07082d7152db43627d52b5f126e9e39886b8d
Maximino Reyna <temporicide28@gmail.com>
2017-10-25 14:55:09 -0500

    set up the beginning collected variables that will be used in test

--- src/final_project/MedicalProfile/Medical.java
+++ src/final_project/MedicalProfile/Medical.java
@@ -13,9 +13,18 @@ public class Medical {
     /**
      * Add all values one can make medical inferences from 
      * that's not allergies
+     * 
+     * Maybe make into activation switches for more options?
      */
-    
-    
+    public String drugAbuse;
+    public String alcoholAbuse;
+    public String heartProblems;
+    public String digestiveDisorder;
+    public String onMedication;// might want to make this a switch for 
+                                //multiple strings of medications
+    public String familyDiabetes;
+    public String personalDiabetes;// maybe a switch to also require 
+                                    //the severity?
     
     
     
--- src/final_project/MedicalProfile/MedicalGUI.java
+++ src/final_project/MedicalProfile/MedicalGUI.java
@@ -6,16 +6,71 @@
 package final_project.MedicalProfile;
 
 import javafx.scene.control.TextField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 /**
  *
  * @author MaxIII
  */
 public class MedicalGUI {
-    static public TextField drugAbuseTF = new TextField();//switch
-    static public TextField alcoholTF = new TextField();
-    static public TextField heartProblemsTF = new TextField();//switch
-    static public TextField digestiveDisorderTF = new TextField();//switch
-    static public TextField onOtherMedsTF = new TextField();
-    static public TextField familyDiabetesTF = new TextField();
-    static public TextField personalDiabetesTF = new TextField();
+    static public RadioButton drugAbuseRBy = new RadioButton("Yes");
+    static public RadioButton drugAbuseRBn = new RadioButton("No");
+    public static final ToggleGroup drugAbuseTG = new ToggleGroup();
+    static public RadioButton alcoholRBy = new RadioButton("Yes");
+    static public RadioButton alcoholRBn = new RadioButton("No");
+    public static final ToggleGroup alcoholTG = new ToggleGroup();
+    static public RadioButton heartProblemsRBy = new RadioButton("Yes");
+    static public RadioButton heartProblemsRBn = new RadioButton("No");
+    public static final ToggleGroup heartProblemsTG = new ToggleGroup();
+    static public RadioButton digestiveDisorderRBy = new RadioButton("Yes");
+    static public RadioButton digestiveDisorderRBn = new RadioButton("No");
+    public static final ToggleGroup digestiveDisorderTG = new ToggleGroup(); 
+    static public RadioButton onOtherMedsRBy = new RadioButton("Yes");//switch
+    static public RadioButton onOtherMedsRBn = new RadioButton("No");
+    public static final ToggleGroup onOtherMedsTG = new ToggleGroup();
+    static public RadioButton familyDiabetesRBy = new RadioButton("Yes");
+    static public RadioButton familyDiabetesRBn = new RadioButton("No");
+    public static ToggleGroup familyDiabetesTG = new ToggleGroup();
+    static public RadioButton personalDiabetesRBy = new RadioButton("Yes");
+    static public RadioButton personalDiabetesRBn = new RadioButton("No");
+    public static ToggleGroup personalDiabetesTG = new ToggleGroup();
+    
+    
+    public static void setToggles(){
+        drugAbuseRBy.setToggleGroup(drugAbuseTG);
+        drugAbuseRBn.setToggleGroup(drugAbuseTG);
+        alcoholRBy.setToggleGroup(alcoholTG);
+        alcoholRBn.setToggleGroup(alcoholTG);
+        heartProblemsRBy.setToggleGroup(heartProblemsTG);
+        heartProblemsRBn.setToggleGroup(heartProblemsTG);
+        digestiveDisorderRBy.setToggleGroup(digestiveDisorderTG);
+        digestiveDisorderRBn.setToggleGroup(digestiveDisorderTG);
+        onOtherMedsRBy.setToggleGroup(onOtherMedsTG);
+        onOtherMedsRBn.setToggleGroup(onOtherMedsTG);
+        familyDiabetesRBy.setToggleGroup(familyDiabetesTG);
+        familyDiabetesRBn.setToggleGroup(familyDiabetesTG);
+        personalDiabetesRBy.setToggleGroup(personalDiabetesTG);
+        personalDiabetesRBn.setToggleGroup(personalDiabetesTG);
+    }
+    
+    
+    public static void resetMedical(){
+        drugAbuseRBy.setSelected(false);
+        drugAbuseRBn.setSelected(false);
+        alcoholRBy.setSelected(false);
+        alcoholRBn.setSelected(false);
+        heartProblemsRBy.setSelected(false);
+        heartProblemsRBn.setSelected(false);
+        digestiveDisorderRBy.setSelected(false);
+        digestiveDisorderRBn.setSelected(false);
+        onOtherMedsRBy.setSelected(false);;
+        onOtherMedsRBn.setSelected(false);
+        familyDiabetesRBy.setSelected(false);
+        familyDiabetesRBn.setSelected(false);
+        personalDiabetesRBy.setSelected(false);
+        personalDiabetesRBn.setSelected(false);
+    
+    }
+    
+    
 }
