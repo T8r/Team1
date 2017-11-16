07f62555d1e2b5856708d7976b8f71d13a56cf28
Jacob Shircliff <jacobshircliff@yahoo.com>
2017-11-16 07:27:51 -0600

    Added Email section

---
+++ VAQ_Health/src/Email/EmailGUI.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Email;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author JacobS
+ */
+public class EmailGUI extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        jEmailModel model = new jEmailModel();
+        model.setHost("smtp.mail.gmail.com");
+        model.setPassword("");
+        model.setPort("465");
+        model.setSubject("Sending File");
+        model.setToAddress("");
+        model.setUserName("");
+        model.setMessage("File sent.");
+        jEmailView root = new jEmailView();
+        jEmailController controller = new jEmailController(root, model);
+        //this information ought to be gleaned from outside the program
+
+        
+        Scene scene = new Scene(root, 650, 450);
+        
+        primaryStage.setTitle("Email GUI");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
